3ed39710d53faabb3133388b4dfb82a83fae623f
RestuHarmoni <restuharmoni@gmail.com>
2026-06-01 11:29:23 +0800

    Delete supabase directory

--- supabase/config.js
+++
@@ -1,5 +0,0 @@
-// JawiKids Supabase Config v1.04 LOGIN FIX
-// Safe frontend key only. Jangan letak service_role key di frontend.
-
-window.JAWIKIDS_SUPABASE_URL = "https://tuwbamxobhkbbkaodvkg.supabase.co";
-window.JAWIKIDS_SUPABASE_ANON_KEY = "sb_publishable_zuXeYtmjbpYaFCJq9Ker7A_ngs5_VOu";
